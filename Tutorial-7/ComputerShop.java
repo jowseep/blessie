785ffcdaf994a81ff70317e95b36335689eff7e7
Joseph Callao <48220517+jowseep@users.noreply.github.com>
2022-12-22 16:32:31 +0800

    added required methods

--- Tutorial-7/ComputerShop.java
+++ Tutorial-7/ComputerShop.java
@@ -2,15 +2,55 @@ import java.util.ArrayList;
 
 public class ComputerShop {
 
-    public ComputerShop(ArrayList<Part> parts) {}
-
-    public ArrayList<Part> createArray(Part part) {
+    public void createList(Part part) {
         ArrayList<Part> parts = new ArrayList<>();
         parts.add(part);
-        return parts;
     }
 
-    
-    
+    public void loadPartsData(String pathName, ArrayList<Part> parts) {
+        try {
+            File filePath = new File(pathName);
+            Scanner fileInfo = new Scanner(filePath);
+
+            while(fileInfo.hasNextLine()) {
+                Part part = new Part();
+                part.setName(fileInfo.nextLine());
+                part.setDescription(fileInfo.nextLine());
+                part.setPrice(Double.parseDouble(fileInfo.nextLine()));
+                part.setQuantityInStock(5);
+                parts.add(part);
+            }
+            fileInfo.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public void showParts(ArrayList<Part> parts) {
+        for(Part sumn:parts) {
+            System.out.println(sumn.toString());
+            System.out.println();
+        }
+    }
+
+    public void reportParts(String fileName, ArrayList<Part> parts) throws FileNotFoundException {
+            File fw = new File(fileName);
+            PrintWriter output = new PrintWriter(fw);
+            
+            for(Part sumn:parts) {
+                output.println(sumn);
+            }
+            output.close();
+    }
+
+
+    public void addPart(Part part, ArrayList<Part> parts) {
+        if(part == null) {
+            System.out.println("Empty object, hoe.");
+        } else {
+            parts.add(part);
+        }
+    }
     
 }
